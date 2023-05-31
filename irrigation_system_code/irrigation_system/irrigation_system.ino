8f538503293ee75f373263e78e367d0d85a3e667
Juan Pablo <jpablo.velozachaves@gmail.com>
2023-05-30 21:33:19 -0500

    ultima version del codigo

--- irrigation_system_code/irrigation_system/irrigation_system.ino
+++ irrigation_system_code/irrigation_system/irrigation_system.ino
@@ -8,13 +8,14 @@ int D5 = 10;
 int D6 = 11;
 int D7 = 12;
 
-int waterlevel = A0;
-int datawaterlevel;
+//int waterlevel = A0;
+//int datawaterlevel;
 int soilmoisture = A1;
 int datasoilmoisture;
 int blueled = 2;
 int greenled = 4;
 int yellowled = 7;
+int waterbomb = 8;
 
 LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
 
@@ -22,43 +23,45 @@ void setup() {
   lcd.begin(16, 2);
   analogWrite(V0, 50);
   Serial.begin(9600);
-  pinMode(waterlevel, INPUT);
+  //pinMode(waterlevel, INPUT);
   pinMode(soilmoisture, INPUT);
   pinMode(blueled, OUTPUT);
   pinMode(greenled, OUTPUT);
   pinMode(yellowled, OUTPUT);
+  pinMode(waterbomb, OUTPUT);
 
 }
 void loop() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("N.agua: ");
-  lcd.print(datawaterlevel);
-  lcd.print("%");
+  //lcd.print("N.agua: ");
+  //lcd.print(datawaterlevel);
+  //lcd.print("%");
+  lcd.print("Proy. grado");
   lcd.setCursor(0, 1);
   lcd.print("H.tierra: ");
   lcd.print(datasoilmoisture);
   lcd.print("%");
   
-  datawaterlevel = analogRead(waterlevel);
-  Serial.print("Nivel del agua: ");
-  Serial.println(datawaterlevel);
-  delay(1500);
+  //datawaterlevel = analogRead(waterlevel);
+  //Serial.print("Nivel del agua: ");
+  //Serial.println(datawaterlevel);
+  //delay(1500);
 
   datasoilmoisture = analogRead(soilmoisture);
   Serial.print("Humedad del suelo: ");
   Serial.println(datasoilmoisture);
-  delay(1500);
+  delay(1000);
 
- if (datawaterlevel >= 610 && datawaterlevel <= 1023) {
-  digitalWrite(blueled, HIGH);
+ if (datasoilmoisture >= 750 && datasoilmoisture <= 1023) {
+  digitalWrite(yellowled, HIGH);
  }
 
  else{
-  digitalWrite(blueled, LOW);
+  digitalWrite(yellowled, LOW);
  }
 
- if (datawaterlevel >= 490 && datawaterlevel <= 609) {
+ if (datasoilmoisture >= 450 && datasoilmoisture <= 749) {
   digitalWrite(greenled, HIGH);
  }
 
@@ -66,11 +69,20 @@ void loop() {
   digitalWrite(greenled, LOW);
  }
 
- if (datawaterlevel >= 0 && datawaterlevel <= 489) {
-  digitalWrite(yellowled, HIGH);
+ if (datasoilmoisture >= 0 && datasoilmoisture <= 449) {
+  digitalWrite(blueled, HIGH);
  }
 
  else{
-  digitalWrite(yellowled, LOW);
+  digitalWrite(blueled, LOW);
+ }
+
+ if(datasoilmoisture >= 850 && datasoilmoisture <= 1023) {
+  digitalWrite(waterbomb, HIGH);
+  delay(6000);
+ }
+
+ else{
+  digitalWrite(waterbomb, LOW);
  }
 }
